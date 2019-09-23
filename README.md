9ba131768af3483026d9115fafe1c18c378e526b
Hoàng Kim Minh <41377837+hoangkimminh@users.noreply.github.com>
2019-09-23 14:18:32 +0700

    [features/f16] Set up user-manager (#1)

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# user-manager
-Night Watch user manager
+# Night Watch user manager
+
+## INSTALLATION
+
+### Requirements
+
+- Node.js >= 8
+- MongoDB >= 3
+- Dotenv files: `.env.production` and/or `.env.development`
+
+### Instructions
+
+```bash
+$ yarn install
+$ yarn start # yarn dev for development
+```
+
+## DOCUMENTATION
+
+### Environment Variables
+
+- `PORT` (number): Port number to run the server
+- `MONGODB_URI` (string): MongoDB URI
+
+### Routes
+
+#### GET `/`
+
+> Just for testing
+
+##### Response body
+
+- `iam`: `"/"`
+
+#### GET `/users`
+
+> Get all users
+
+##### Response body
+
+Array of objects:
+
+- `_id` (string): ID of the user
+- `username` (string): Username
+- `name` (string): User's full name
+- `email` (string): User's email
+- `birthday` (string): User's birthday
+- `createdAt` (object): Time at which the user was created
+- `updatedAt` (object): Time at which the user was updated
+
+#### POST `/users`
+
+> Add a new user
+
+##### Request body
+
+- `username` (string): Username
+- `name` (string): User's fullname
+- `email` (string): User's email
+- `birthday` (string): User's birthday
+
+#### GET `/users/:id`
+
+> Get the user with `id`
+
+##### Route parameters
+
+- `id` (string): ID of the user
+
+##### Response body
+
+- `_id` (string): ID of the user
+- `username` (string): Username
+- `name` (string): User's fullname
+- `email` (string): User's email
+- `birthday` (string): User's birthday
+- `createdAt` (object): Time at which the user was created
+- `updatedAt` (object): Time at which the user was updated
+
+#### DELETE `/users/:id`
+
+> Delete the user with `id`
+
+##### Route parameters
+
+- `id` (string): ID of the user
---
+++ src/server.js
@@ -0,0 +1,94 @@
+require('dotenv-flow').config()
+
+const fastify = require('fastify')
+const mongo = require('mongodb')
+
+const loggerLevel = process.env.NODE_ENV !== 'production' ? 'debug' : 'info'
+const server = fastify({ ignoreTrailingSlash: true, logger: { level: loggerLevel } })
+
+const { getCollection } = require('./database')
+
+/**
+ * Get all users
+ */
+server.get('/users', async (req, res) => {
+  try {
+    const Users = await getCollection('users')
+    let result = await Users.find({}).toArray()
+    res.code(200).send(result)
+  } catch (err) {
+    res.code(500)
+  }
+})
+
+/**
+ * Add an user
+ */
+server.post('/users', async (req, res) => {
+  const { username, name, email, birthday } = req.body
+  try {
+    const Users = await getCollection('users')
+    await Users.insertOne({
+      username: username,
+      name: name,
+      email: email,
+      birthday: birthday,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    res.code(204)
+  } catch (err) {
+    res.code(500)
+  }
+})
+
+/**
+ * Get an user with id
+ */
+server.get('/users/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const Users = await getCollection('users')
+    let result = await Users.findOne({ _id: new mongo.ObjectID(id) })
+    res.code(200).send(result)
+  } catch (err) {
+    res.code(500)
+  }
+})
+
+/**
+ * Update an user with id
+ */
+server.post('/users/update/:id', async (req, res) => {
+  const id = req.params.id
+  const { username, name, email, birthday } = req.body
+  try {
+    const Users = await getCollection('users')
+    await Users.updateOne(
+      { _id: new mongo.ObjectID(id) },
+      {
+        $set: {
+          username: username,
+          name: name,
+          email: email,
+          birthday: birthday,
+          updatedAt: new Date()
+        }
+      }
+    )
+    res.code(204)
+  } catch (err) {
+    res.code(500)
+  }
+})
+
+const start = async () => {
+  try {
+    server.listen(process.env.PORT, '::') // listen to all IPv6 and IPv4 addresses
+  } catch (err) {
+    server.log.error(err.message)
+    process.exit(1)
+  }
+}
+
+start()
